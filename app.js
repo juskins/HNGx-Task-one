da0d295802bab6d810ba78857cc506d9a5ef5970
Juskins <omojuwababatunde1@gmail.com>
2023-09-08 14:42:21 +0100

    updated

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
        const dayOfWeekElement = document.querySelector('[data-testid="currentDayOfTheWeek"]');
        const utcTimeElement = document.querySelector('[data-testid="currentUTCTime"]');
    
-       // Get the current day of the week
+       //current day of the week
        const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
        const today = new Date();
        const dayOfWeek = daysOfWeek[today.getUTCDay()];
@@ -12,7 +12,6 @@ document.addEventListener("DOMContentLoaded", function () {
        const utcOffsetMillis = 1 * 60 * 60 * 1000; // UTC+1 in milliseconds
        const watUtcTime = new Date().getTime() + utcOffsetMillis;
    
-       // Update the elements with the values
        dayOfWeekElement.textContent = `Current Day of the Week: ${dayOfWeek}`;
        utcTimeElement.textContent = `Current UTC Time (WAT): ${watUtcTime} milliseconds`;
    });
