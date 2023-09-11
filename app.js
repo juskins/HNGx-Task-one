a9e1602ae2f9289a5b49c9fd8ab715fc807b4356
Juskins <omojuwababatunde1@gmail.com>
2023-09-11 04:21:20 +0100

    updated

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 // script.js
 document.addEventListener("DOMContentLoaded", function () {
-       const dayOfWeekElement = document.querySelector('[data-testid="currentDayOfTheWeek"]');
-       const utcTimeElement = document.querySelector('[data-testid="currentUTCTime"]');
-   
-       //current day of the week
-       const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-       const today = new Date();
-       const dayOfWeek = daysOfWeek[today.getUTCDay()];
-   
-       // Calculate the current UTC time in milliseconds for WAT (UTC+1)
-       const utcOffsetMillis = 1 * 60 * 60 * 1000; // UTC+1 in milliseconds
-       const watUtcTime = new Date().getTime() + utcOffsetMillis;
-   
-       dayOfWeekElement.textContent = `Current Day of the Week: ${dayOfWeek}`;
-       utcTimeElement.textContent = `Current UTC Time (WAT): ${watUtcTime} milliseconds`;
-   });
-   
+    const dayOfWeekElement = document.querySelector('[data-testid="currentDayOfTheWeek"]');
+    const utcTimeElement = document.querySelector('[data-testid="currentUTCTime"]');
+
+    // Get the current day of the week
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const today = new Date();
+    const dayOfWeek = daysOfWeek[today.getUTCDay()];
+
+    // Get the current UTC time in milliseconds
+    const utcTime = today.getTime();
+
+    // Update the elements with the values
+    dayOfWeekElement.textContent = `Current Day of the Week: ${dayOfWeek}`;
+    utcTimeElement.textContent = `Current UTC Time: ${utcTime} milliseconds`;
+});
